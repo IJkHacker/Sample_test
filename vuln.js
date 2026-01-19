7244b15c9c995300e5762608b44202b51f12b2ce
IJkHacker <161743070+IJkHacker@users.noreply.github.com>
2026-01-19 11:25:29 +0530

    Create vuln.js

---
+++ vuln.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { exec } = require("child_process");
+
+const app = express();
+
+app.get("/run", (req, res) => {
+  const cmd = req.query.cmd;
+  exec(cmd, (err, output) => {
+    res.send(output);
+  });
+});
+
+app.listen(3000);
